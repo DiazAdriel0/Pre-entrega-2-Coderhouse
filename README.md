830598230379d67c8460bd79c86e673f8a6261e9
DiazAdriel0 <diazadriel0@gmail.com>
2023-04-05 07:58:41 -0300

    pre entrega 2 completa

---
+++ README.md
@@ -0,0 +1,5 @@
+Ejercicio 2 de pre entrega para Coderhouse
+
+El script "simula" un carrito de compras, donde los productos solo pueden ser agregados 1 vez.
+Cree bucles para validar si el usuario ingresa correctamente o no los datos pedidos para agregar los productos
+Utilicé class, objetos, arrays, arrays de objetos, metodos de objetos, metodos de arrays, bucles for y while y condicionales.
---
+++ script.js
@@ -0,0 +1,61 @@
+let productos = [];
+class Productos {
+    constructor(nombre,precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.vendido = false;
+    }
+    vender() {
+        this.vendido = true;
+    }
+}
+
+const producto1 = new Productos("Mouse Logitech G203",10000);
+const producto2 = new Productos("Teclado HyperX Alloy FPS pro",15000);
+const producto3 = new Productos("Auriculares ReDragon Scylla",8000);
+const producto4 = new Productos("Monitor Asus TUF 144Hz",119000);
+const producto5 = new Productos("Micrófono HyperX Quadcast",110000);
+const producto6 = new Productos("Silla Corsair T3 Rush",270000);
+
+productos.push(producto1,producto2,producto3,producto4,producto5,producto6);
+
+let elegidoPorElUsuario = prompt(`¿Que producto quiere agregar al carrito?\n1- ${productos[0].nombre} -- Precio: $${productos[0].precio}\n2- ${productos[1].nombre} -- Precio: $${productos[1].precio}\n3- ${productos[2].nombre} -- Precio: $${productos[2].precio}\n4- ${productos[3].nombre} -- Precio: $${productos[3].precio}\n5- ${productos[4].nombre} -- Precio: $${productos[4].precio}\n6- ${productos[5].nombre} -- Precio: $${productos[5].precio}\n\nEscriba solo los numeros separados por un espacio\nPor ejemplo: 1 3 5\nLos productos NO se pueden repetir`)
+let arrElegidos = elegidoPorElUsuario.trim().split(" ");
+let validador = false;
+//Si el usuario no ingresó correctamente los productos, a validador lo convierto en true
+for (let i = 0; i < arrElegidos.length; i++) {
+    const element = Number(arrElegidos[i]);
+    if ((element < 0) || (element > 6) || (isNaN(element)) || (arrElegidos[i] == "") || (arrElegidos.length > 6)) {
+        validador = true;
+    }
+}
+//Acá uso el validador para que el usuario ingrese correctamente los productos que quiere
+while (validador){
+    alert("No ingresó productos válidos")
+    elegidoPorElUsuario = prompt(`Vuelva a ingresar los productos (lea la forma correcta de ingresarlos abajo)\nRecuerde que por pedido solo puede agregar 1 producto de cada uno\n1- ${productos[0].nombre} -- Precio: $${productos[0].precio}\n2- ${productos[1].nombre} -- Precio: $${productos[1].precio}\n3- ${productos[2].nombre} -- Precio: $${productos[2].precio}\n4- ${productos[3].nombre} -- Precio: $${productos[3].precio}\n5- ${productos[4].nombre} -- Precio: $${productos[4].precio}\n6- ${productos[5].nombre} -- Precio: $${productos[5].precio}\n\nEscriba solo los numeros separados por un espacio\nPor ejemplo: 1 3 5\nLos productos NO se pueden repetir`)
+    arrElegidos = elegidoPorElUsuario.trim().split(" ");
+    validador = false;
+    for (let i = 0; i < arrElegidos.length; i++) {
+        const element = Number(arrElegidos[i]);
+        if ((element < 0) || (element > 6) || (isNaN(element)) || (arrElegidos[i] == "") || (arrElegidos.length > 6)) {
+            validador = true;
+        }
+    }
+}
+
+for (let i = 0; i < arrElegidos.length; i++) {
+    const element = Number(arrElegidos[i]);
+    productos[element - 1].vender();
+}
+
+let productosElegidos = productos.filter( (elem) => elem.vendido === true);
+let stringDeElegidos = "";
+productosElegidos.forEach(element => {
+    return stringDeElegidos = stringDeElegidos + "- " + element.nombre + "\n";
+});
+let totalAPagar = productosElegidos.reduce((acc,producto) => acc + producto.precio, 0);
+if(productosElegidos.length == 1){
+    alert(`Los producto elegido es:\n\n${stringDeElegidos}\nEl total a pagar por el producto es: $${totalAPagar}`)
+}else {
+    alert(`Los productos elegidos son:\n\n${stringDeElegidos}\nEl total a pagar por los ${productosElegidos.length} productos es: $${totalAPagar}`)
+}
